7b988c210f35abae1eb1eb013faa4269fb9a5c84
Fabian Wrede <fabian.wrede@wi.uni-muenster.de>
2018-06-10 22:15:38 +0200

    fix get_game_of_teams method; problem led to repeated matches

--- src/controller/helper.py
+++ src/controller/helper.py
@@ -10,7 +10,7 @@ def get_games_of_team(tournament, team):
 def get_game_of_teams(tournament, team_a, team_b):
     for r in tournament.rounds:
         for g in r.games:
-            if (g.team_a == team_a.name and g.team_b is team_b.name) or (
+            if (g.team_a == team_a.name and g.team_b == team_b.name) or (
                     g.team_b == team_a.name and g.team_a == team_b.name):
                 return g
     return None
