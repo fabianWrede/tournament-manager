aef2e75b19ee5fb1e510fe058932602bfdc0f70f
Fabian Wrede <fabian.wrede@wi.uni-muenster.de>
2018-06-11 11:10:32 +0200

    install required packages for texlive

--- snap/plugins/x-texlive.py
+++ snap/plugins/x-texlive.py
@@ -1,4 +1,5 @@
 # https://github.com/ubuntu/snappy-playpen/blob/master/texworks/parts/plugins/x-texlive.py
+# https://github.com/otfried/ipe-tools/blob/master/snap/snap/plugins/x-texlive.py
 
 import os
 from subprocess import Popen, PIPE
@@ -7,6 +8,8 @@ from subprocess import Popen, PIPE
 import snapcraft
 from snapcraft.plugins import dump
 
+extras = ["koma-script", "lmodern", "microtype", "booktabs", "ltablex"]
+
 class TexLivePlugin(snapcraft.plugins.dump.DumpPlugin):
 
     def build(self):
@@ -14,13 +17,15 @@ class TexLivePlugin(snapcraft.plugins.dump.DumpPlugin):
 
         # Install TexLive with the standard installer
         env = self._build_environment()
-        #self.run(['{}/install-tl'.format(self.builddir), '-scheme', 'basic'], env=env)
         p1 = Popen(['echo', '-n', 'I'], env=env, stdout=PIPE)
-        p2 = Popen(['{}/install-tl'.format(self.builddir), '-portable', '-scheme', 'basic'], env=env, stdin=p1.stdout, stdout=PIPE)
+        p2 = Popen(['{}/install-tl'.format(self.builddir), '-portable',
+                 '-scheme', 'basic'], env=env, stdin=p1.stdout, stdout=PIPE)
         output = p2.communicate()[0]
-
+        self.run(['{}/texlive/bin/x86_64-linux/tlmgr'.format(self.installdir),
+                  'install'] + extras, env=env)
 
     def _build_environment(self):
         env = os.environ.copy()
-        env['TEXLIVE_INSTALL_PREFIX'] = os.path.join(self.installdir, 'texlive')
+        env['TEXLIVE_INSTALL_PREFIX'] = os.path.join(self.installdir,
+                                                     'texlive')
         return env
