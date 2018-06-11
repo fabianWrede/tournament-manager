c5aa3a0f845b063b1959fe00424703086055f5c6
Fabian Wrede <fabian.wrede@wi.uni-muenster.de>
2018-06-11 11:59:49 +0200

    texlive installation for all platforms (i386, arm64 ...)

--- snap/plugins/x-texlive.py
+++ snap/plugins/x-texlive.py
@@ -21,8 +21,22 @@ class TexLivePlugin(snapcraft.plugins.dump.DumpPlugin):
         p2 = Popen(['{}/install-tl'.format(self.builddir), '-portable',
                  '-scheme', 'basic'], env=env, stdin=p1.stdout, stdout=PIPE)
         output = p2.communicate()[0]
-        self.run(['{}/texlive/bin/x86_64-linux/tlmgr'.format(self.installdir),
-                  'install'] + extras, env=env)
+
+        base_path = os.path.join(self.installdir, 'texlive', 'bin')
+        
+        tlmgr_path_x86_64 = os.path.join(base_path, 'x86_64-linux', 'tlmgr')
+        tlmgr_path_i386 = os.path.join(base_path, 'i386-linux', 'tlmgr')
+        tlmgr_path_armhf = os.path.join(base_path, 'armhf-linux', 'tlmgr')
+        tlmgr_path_arm64 = os.path.join(base_path, 'aarch64-linux', 'tlmgr')
+
+        if os.path.exists(tlmgr_path_x86_64):
+            self.run([tlmgr_path_x86_64, 'install'] + extras, env=env)
+        elif os.path.exists(tlmgr_path_i386):
+            self.run([tlmgr_path_i386, 'install'] + extras, env=env)
+        elif os.path.exists(tlmgr_path_armhf):
+            self.run([tlmgr_path_armhf, 'install'] + extras, env=env)
+        elif os.path.exists(tlmgr_path_arm64):
+            self.run([tlmgr_path_arm64, 'install'] + extras, env=env)       
 
     def _build_environment(self):
         env = os.environ.copy()
